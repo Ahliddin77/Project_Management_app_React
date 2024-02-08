8bc8ef496930d404e851ab196acd2abf52fd465f
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2024-02-08 20:31:00 +0100

    add NewProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
