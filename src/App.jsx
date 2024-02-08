c25d0e2f4dd2c1cb1c496fb1ed64a9ece943cde3
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2024-02-08 20:56:38 +0100

    add NoProjectSelected component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 export default function ProjectSidebar() {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -5,9 +7,7 @@ export default function ProjectSidebar() {
         Your Projects
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-          + Add Project
-        </button>
+        <Button>+ Add Project</Button>
       </div>
       <ul></ul>
     </aside>
