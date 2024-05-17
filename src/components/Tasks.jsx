4b20bc930e52fd49c082441e931121b73e368c8b
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2024-05-17 19:12:02 +0200

    drop dwon completed taks in the list

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,18 +1,27 @@
 import NewTask from "./NewTask";
+import { useState, useEffect } from "react";
 
 export default function Tasks({ tasks, onAdd, onDelete, onToggleStatus }) {
+  const [sortedTasks, setSortedTasks] = useState(tasks);
+
+  useEffect(() => {
+    const completedTasks = tasks.filter((task) => task.completed);
+    const incompleteTasks = tasks.filter((task) => !task.completed);
+    setSortedTasks([...incompleteTasks, ...completedTasks]);
+  }, [tasks]);
+
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <NewTask onAdd={onAdd} />
-      {tasks.length === 0 && (
+      {sortedTasks.length === 0 && (
         <p className="text-stone-800 my-4">
           This project does not have any tasks yet.
         </p>
       )}
-      {tasks.length > 0 && (
+      {sortedTasks.length > 0 && (
         <ul className="p-4 mt-8 rounded-md bg-stone-100">
-          {tasks.map((task) => (
+          {sortedTasks.map((task) => (
             <li
               className="flex justify-between items-center my-4"
               key={task.id}
